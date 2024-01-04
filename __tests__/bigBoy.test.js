7ea49422049d8236578eb369fd7c6c2ffd3c30b4
Will <billyjoe2701@gmail.com>
2024-01-04 10:18:36 +0000

    Patching a post

--- __tests__/bigBoy.test.js
+++ __tests__/bigBoy.test.js
@@ -1,4 +1,4 @@
-const { deletePostsById } = require("../src/app/api/posts/route");
+const { deletePostsById, patchPostById } = require("../src/app/api/posts/[id]/route");
 const seedDatabase = require("../seed/seed");
 
 beforeEach(async () => {
@@ -9,28 +9,61 @@ afterAll(async () => {
   prisma.$disconnect();
 });
 
-describe('Delete a Jimmy', ()=>{
-    test('204 - Deletes a jimmy and returns nothing', async ()=>{
-      const response = await deletePostsById(1)
-      expect(response.status).toBe(200)
-  
-      const deletePost = await response.json()
-      expect(deletePost).toEqual({})
+describe("Delete a Jimmy", () => {
+  test("204 - Deletes a jimmy and returns nothing", async () => {
+    const response = await deletePostsById(1);
+    expect(response.status).toBe(200);
+
+    const deletePost = await response.json();
+    expect(deletePost).toEqual({});
+  });
+
+  test("400 - Incorrect Data Type", async () => {
+    const response = await deletePostsById("Banana");
+    expect(response.status).toBe(400);
+
+    const err = await response.json();
+    expect(err).toBe("Incorrect Data Type");
+  });
+
+  test("404 - No Post Found", async () => {
+    const response = await deletePostsById(99);
+    expect(response.status).toBe(404);
+
+    const err = await response.json();
+    expect(err).toBe("No posts found");
+  });
+});
+
+describe('Patching a post By Id', ()=>{
+    test('200 - Patches Post and return updated post', async ()=>{
+        const response = await patchPostById(1)
+        expect(response.status).toBe(200)
+
+        const post = await response.json()
+        expect(post.posts).toMatchObject({
+            post_id: 1,
+            likes: 4,
+            session_name: 'afternoon pump baby!!',
+            description: 'feeling huge now',
+            session_id: 1,
+            user_id: 1
+        })
     })
-  
+
     test('400 - Incorrect Data Type', async ()=>{
-      const response = await deletePostsById("Banana")
-      expect(response.status).toBe(400)
-  
-      const err = await response.json()
-      expect(err).toBe('Incorrect Data Type')
+        const response = await patchPostById("Banana");
+        expect(response.status).toBe(400);
+
+        const err = await response.json();
+        expect(err).toBe('Incorrect Data Type');
     })
-  
-    test('404 - No Post Found', async ()=>{
-      const response = await deletePostsById(99)
-      expect(response.status).toBe(404)
-  
-      const err = await response.json()
-      expect(err).toBe('No posts found')
+
+    test('404 - Post not found', async ()=>{
+        const response = await patchPostById(99);
+        expect(response.status).toBe(404);
+
+        const err = await response.json();
+        expect(err).toBe('No posts found');
     })
-  })
+})
