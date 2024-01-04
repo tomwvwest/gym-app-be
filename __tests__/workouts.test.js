01aa5238cf43c271d3d1947a20d4f08f5fb4321e
harrycs1 <98349746+harrycs1@users.noreply.github.com>
2024-01-04 17:39:29 +0000

    this commit adds the getWorkoutsByCreatorId endpoint

--- __tests__/workouts.test.js
+++ __tests__/workouts.test.js
@@ -1,4 +1,4 @@
-const { getWorkouts, postWorkout, patchWorkout, deleteWorkout } = require("../src/app/api/workouts/route");
+const { getWorkouts, getWorkoutsByCreatorId, postWorkout, deleteWorkout, checkUserExists } = require("../src/app/api/workouts/route");
 const seedDatabase = require("../seed/seed");
 
 beforeEach(async () => {
@@ -8,6 +8,29 @@ beforeEach(async () => {
 afterAll(async () => {
   prisma.$disconnect();
 });
+
+describe("Utility functions", () => {
+    test("checkUserExists returns no error for existing user_id", async () => {
+        expect.assertions(0)
+        return checkUserExists(1)
+    });
+
+    test("checkUserExists returns appropriate status and error message for non-existent user_id", async () => {
+        expect.assertions(2)
+        return checkUserExists(999).catch(error => {
+            expect(error.message).toMatch('User does not exist.')
+            expect(error.status).toBe(404)
+        })
+    });
+
+    test("checkUserExists returns appropriate status and error message for invalid user_id", async () => {
+        expect.assertions(2)
+        return checkUserExists('banana').catch(error => {
+            expect(error.message).toMatch('Bad Request.')
+            expect(error.status).toBe(400)
+        })
+    });
+})
 
 describe("GET workouts", () => {
   test("GET:200 returns a list of workouts", async () => {
@@ -22,6 +45,37 @@ describe("GET workouts", () => {
         creator_id: expect.any(Number)
       });
     });
+  });
+
+  test("GET:200 returns a list of workouts by creator_id", async () => {
+    const response = await getWorkoutsByCreatorId(1);
+    expect(response.status).toBe(200);
+
+    const workouts = await response.json();
+    workouts.forEach((workout) => {
+      expect(workout).toEqual({
+        workout_id: expect.any(Number),
+        workout_name: expect.any(String),
+        creator_id: 1
+      });
+    });
+  });
+
+  test("GET:404 sends an appropriate status and error message when provided with a non-existent creator_id", async () => {
+    const response = await getWorkoutsByCreatorId(999);
+    // console.log(response)
+    expect(response.status).toBe(404);
+
+    const workouts = await response.json();
+    expect(workouts.message).toBe('User does not exist.');
+  });
+
+  test("GET:400 sends an appropriate status and error message when provided with a invalid creator_id", async () => {
+    const response = await getWorkoutsByCreatorId('banana');
+    expect(response.status).toBe(400);
+
+    const workouts = await response.json();
+    expect(workouts.message).toBe('Bad Request.');
   });
 });
 
@@ -48,5 +102,25 @@ describe("POST workout", () => {
 
     const workouts = await response.json();
     expect(workouts.message).toBe('Post failed. User does not exist.');
+  });
+
+  test("POST:400 sends an appropriate status and error message when provided with an incomplete workout (no workout_name)", async () => {
+    const newWorkout = { workout_name: "", creator_id: 1 };
+
+    const response = await postWorkout(newWorkout);
+    expect(response.status).toBe(400);
+
+    const workouts = await response.json();
+    expect(workouts.message).toBe('Bad request.');
+  });
+
+  test("POST:400 sends an appropriate status and error message when provided with an incomplete workout (no creator_id)", async () => {
+    const newWorkout = { workout_name: "newWorkout"};
+
+    const response = await postWorkout(newWorkout);
+    expect(response.status).toBe(400);
+
+    const workouts = await response.json();
+    expect(workouts.message).toBe('Bad request.');
   });
 });
--- src/app/api/workouts/route.js
+++ src/app/api/workouts/route.js
@@ -2,9 +2,18 @@ const { NextResponse } = require("next/server");
 const { Prisma } = require("@prisma/client")
 const { prisma } = require("../../../../lib/prisma");
 
-// async function checkUserExists() {
+async function checkUserExists(id) {
+    if (typeof id !== 'number') {
+        return Promise.reject({status: 400, message: "Bad Request."})
+    }
 
-// }
+    const user = await prisma.users.findUnique({
+        where:{
+            user_id: id
+        }
+    })
+    if (!user) { return Promise.reject({status: 404, message: "User does not exist."}) }
+}
 
 // async function checkWorkoutExists() {
     
@@ -15,10 +24,40 @@ async function getWorkouts() {
     return NextResponse.json(workouts, {status: 200})
 }
 
+async function getWorkoutsByCreatorId(creator_id) {
+    try {        
+        // const userExists = 
+        await checkUserExists(creator_id)
+        // if (!userExists) { throw Error(404) }
+
+        const workouts = await prisma.workouts.findMany({
+            where: {
+                creator_id: creator_id
+            }
+        })
+        return NextResponse.json(workouts, {status: 200})
+
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2003') {
+                error.message = 'Cannot find workouts. User does not exist.'
+                error.status = 404;
+              }
+        } else if (!error.status) {
+            error.status = 400;
+        }
+        return NextResponse.json({message: error.message}, {status: error.status})
+    }
+}
+
 async function postWorkout(workout) {
     const { workout_name, creator_id } = workout;
 
     try {
+        if (!workout_name.length || !creator_id) {
+            throw Error('Bad request.')
+        }
+
         const newWorkout = await prisma.workouts.create({
             data: {
                 workout_name,
@@ -29,31 +68,23 @@ async function postWorkout(workout) {
         return NextResponse.json({newWorkout}, {status: 201})
 
     } catch (error) {
-        let errMessage;
-        let errStatus;
-
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
             if (error.code === 'P2003') {
-                errMessage = 'Post failed. User does not exist.'
-                errStatus = 404;
+                error.message = 'Post failed. User does not exist.'
+                error.status = 404;
               }
-
-            if (error.code === 'P2002') {
-              errMessage = 'relevant error message'
-            }
-
-            return NextResponse.json({message: errMessage}, {status: errStatus})
+        } else {
+            error.status = 400;
         }
+
+        return NextResponse.json({message: error.message}, {status: error.status})
     }
 }
 
-async function patchWorkout(workout) {
-
-}
-
+// delete workout from list of workouts
 async function deleteWorkout(workout) {
 
 }
 
 
-module.exports = {getWorkouts, postWorkout, patchWorkout, deleteWorkout}
+module.exports = {getWorkouts, getWorkoutsByCreatorId, postWorkout, deleteWorkout, checkUserExists}
