1accb5b0993588a05ad77104c170650e9b44b0a1
tomwvwest <tomwvwest@gmail.com>
2024-01-05 10:35:46 +0000

    complete footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import { Title } from "./components/Title";
 
 export default function Home() {
   return (
     <main>
-
+        <Title text={"Trending"}/>
+        
     </main>
   )
 }
