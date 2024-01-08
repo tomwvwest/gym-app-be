1720e2e37689903cc147d5991509cfab2b9e4382
tomwvwest <tomwvwest@gmail.com>
2024-01-08 09:44:58 +0000

    moved getCommentsByPostId

---
+++ src/app/api/posts/[id]/comments/route.js
@@ -0,0 +1,27 @@
+const { NextResponse } = require("next/server");
+const { prisma } = require("../../../../../lib/prisma");
+
+async function GET (req, {params}) {
+  const id = Number(params.id);
+
+  if(isNaN(parseInt(id))){
+      return NextResponse.json('Incorrect Data Type', {status: 400})
+  }
+  const posts = await prisma.posts.findUnique({
+      where: {
+          post_id: id
+      }
+  })
+
+  if(!posts){
+      return NextResponse.json('No posts found', {status: 404})
+  }
+  const comments = await prisma.comments.findMany({
+      where: {
+          post_id: id
+      }
+  })
+  return NextResponse.json(comments, {status: 200})
+}
+
+module.exports = {GET}
