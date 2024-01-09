661fc20db4ed3aa2ce1897d17a2de46ba1496f01
tomwvwest <tomwvwest@gmail.com>
2024-01-09 18:47:56 +0000

    added some loading and error handling and inability to add the same exercise twice

--- src/app/components/Log/LogWorkoutContainer.js
+++ src/app/components/Log/LogWorkoutContainer.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { ListOfExercises } from "./PickExercises";
 import { ChosenExercisesContainer } from "./ChosenExercisesContainer";
 import { LoadingSkeleton } from "../General/LoadingSkeleton";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export const LogWorkoutContainer = () => {
   const router = useRouter();
@@ -11,7 +11,9 @@ export const LogWorkoutContainer = () => {
   const [allExercises, setAllExercises] = useState([]);
   const [isExerciseShowing, setIsExerciseShowing] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [isPostError, setIsPostError] = useState(false)
+  const [isLogLoading, setisLogLoading] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [isPostError, setIsPostError] = useState(false);
   const [workoutData, setWorkoutData] = useState({});
   const [numOfSessions, setNumOfSessions] = useState(null);
 
@@ -32,14 +34,13 @@ export const LogWorkoutContainer = () => {
   }
 
   const handleLogWorkout = () => {
-    setIsLoading(true);
+    setisLogLoading(true);
     const workoutId = 1;
     const userId = 1;
 
     for (const exerciseId in workoutData) {
       Promise.all(
         workoutData[exerciseId].map((set) => {
-
           const newLoggedWorkout = {
             session_id: numOfSessions,
             exercise_id: parseInt(exerciseId),
@@ -58,43 +59,51 @@ export const LogWorkoutContainer = () => {
           });
         })
       )
-        .then((res) => {
-          router.push('/')
+        .then(() => {
+          setisLogLoading(false);
+          setIsLogged(true);
+          setTimeout(() => router.push("/"), 1000);
         })
+        .then(() => {})
         .catch(() => setIsPostError(true));
     }
   };
 
-  if (isLoading) return <LoadingSkeleton/>;
-  if(isPostError) return <>Error, please try again</>
+  if (isLoading) return <LoadingSkeleton />;
 
   return (
     <div className=" my-7 mx-16">
-      <button
-        className="border p-1 rounded relative"
-        onClick={handleAddExerciseButton}
-      >
-        Add Exercise
-        <>
-          {isExerciseShowing ? (
-            <ListOfExercises
-              allExercises={allExercises}
-              setChosenExercises={setChosenExercises}
-              setIsExerciseShowing={setIsExerciseShowing}
-              chosenExercises={chosenExercises}
-            />
-          ) : null}
-        </>
-      </button>
-      <button
-        className={`p-1 ml-4 rounded transition-[0.3s] bg-LightPurple text-platinum ${!chosenExercises.length ? 'opacity-30' : null}`}
-        onClick={handleLogWorkout} disabled={!chosenExercises.length}
-      >
-        Log Workout
-      </button>
-      <button className="border border-DeepPurple p-1 ml-4 rounded bg-LightGreen text-platinum">
-        Log & Post
-      </button>
+      <div className="flex items-center">
+        <button
+          className="border p-1 rounded relative"
+          onClick={handleAddExerciseButton}
+        >
+          Add Exercise
+          <>
+            {isExerciseShowing ? (
+              <ListOfExercises
+                allExercises={allExercises}
+                setChosenExercises={setChosenExercises}
+                setIsExerciseShowing={setIsExerciseShowing}
+                chosenExercises={chosenExercises}
+              />
+            ) : null}
+          </>
+        </button>
+        <button
+          className={`p-1 ml-4 rounded transition-[0.3s] bg-LightPurple text-platinum ${
+            !chosenExercises.length ? "opacity-30" : null
+          }`}
+          onClick={handleLogWorkout}
+          disabled={!chosenExercises.length}
+        >
+          Log Workout
+        </button>
+        <button className="border border-DeepPurple p-1 ml-4 rounded bg-LightGreen text-platinum">
+          Log & Post
+        </button>
+        <p className={`ml-4 italic text-lg ${isLogged ? 'text-LightGreen': null} ${isPostError ? 'text-Red': null}`}>{isLogLoading ? "Logging..." : (isLogged ? 'Success' : (isPostError ? 'Error - try again' : null))}</p>
+      </div>
 
       <div className="z-1">
         <ChosenExercisesContainer
