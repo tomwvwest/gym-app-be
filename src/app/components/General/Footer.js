45121c655716734c30f00d6eebdb277323c8a8bd
tomwvwest <tomwvwest@gmail.com>
2024-01-10 14:26:19 +0000

    exercises footer icon

--- src/app/components/General/Footer.js
+++ src/app/components/General/Footer.js
@@ -4,7 +4,7 @@ import styles from "../../styles.module.css";
 export const Footer = () => {
   return (
     <div className="fixed bottom-0 h-14 border-t border-DeepPurple border-opacity-40 w-full bg-platinum flex items-center">
-      <ul className="w-full justify-between py-3 opacity-75 font-semibold grid grid-cols-4 text-center text-sm">
+      <ul className="w-full justify-between py-3 opacity-75 font-semibold grid grid-cols-5 text-center text-sm">
         <li>
           <Link href="/" className={styles.footerListItem}>
             <img src="../homeIcon.png"></img>
@@ -22,10 +22,16 @@ export const Footer = () => {
           </li>
         </Link>
         <li>
+          <Link href="/exercises" className={styles.footerListItem}>
+            <img src="../exercises.png"></img>Exercises
+          </Link>
+        </li>
+        <li>
           <Link href="/browse" className={styles.footerListItem}>
             <img src="../browseIcon.png"></img>Browse
           </Link>
         </li>
+        
       </ul>
     </div>
   );
