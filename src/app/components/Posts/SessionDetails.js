ef13854c287e8fd38838648365d86c36282de6e3
tomwvwest <tomwvwest@gmail.com>
2024-01-11 12:43:30 +0000

    added sets to home

--- src/app/components/Posts/SessionDetails.js
+++ src/app/components/Posts/SessionDetails.js
@@ -4,17 +4,25 @@ import { SetDetails } from "./SetDetails";
 export const SessionDetails = ({ sessionSets, exerciseObj }) => {
   const exerciseCount = {};
   const exerciseCountToArray = [];
+
   sessionSets.forEach((set) => {
-    if (exerciseCount[set.exercise_id]) exerciseCount[set.exercise_id]++;
-    else exerciseCount[set.exercise_id] = 1;
+    if (exerciseCount[set.exercise_id]) {
+      exerciseCount[set.exercise_id].weight.push(set.weight);
+      exerciseCount[set.exercise_id].reps.push(set.reps);
+    } else {
+      exerciseCount[set.exercise_id] = { weight: [], reps: [] };
+      exerciseCount[set.exercise_id].weight.push(set.weight);
+      exerciseCount[set.exercise_id].reps.push(set.reps);
+    }
   });
 
-  //{exercise : no. of times}
+  //exerciseCount
+  //{5 : {weight:[1,2,3], reps:[1,2,3]},   6 : ...}
   for (const exerciseId in exerciseCount) {
     const correctObj = exerciseObj.find((obj) => {
       return obj.exercise_id === parseInt(exerciseId);
     });
-    exerciseCountToArray.push([correctObj.name, exerciseCount[exerciseId]]);
+    exerciseCountToArray.push([correctObj.name, exerciseCount[exerciseId].weight, exerciseCount[exerciseId].reps]);
   }
   return (
     <>
--- src/app/components/Posts/SetDetails.js
+++ src/app/components/Posts/SetDetails.js
@@ -1,9 +1,20 @@
 import { useState } from "react"
 
 export const SetDetails = ({exercise}) => {
+  //[name, [1,2,3], [11,11,11]]
+  let setInfo = "";
+  for(let i = 0; i<exercise[1].length; i++){
+    if(i=== exercise[1].length -1){
+      setInfo += `${exercise[2][i]} x ${exercise[1][i]}kg `
+    } else{
+      setInfo += `${exercise[2][i]} x ${exercise[1][i]}kg, `
+    }
+  }
+
   return (
-    <>
-      {exercise[1]} x {exercise[0]}
-    </>
+    <div className="mb-2">
+      {exercise[1].length} x {exercise[0]} 
+      <p className="italic ml-1 opacity-80">({setInfo})</p>
+    </div>
   )
 }
