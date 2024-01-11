f37b1942bac4e058217ecf6973bb3b848e23ecb9
jacob <jacobabgarner@gmail.com>
2024-01-11 12:08:07 +0000

    adds dynamic images for users and default image

--- data/users.js
+++ data/users.js
@@ -1,20 +1,28 @@
 module.exports = [{
     "username":"jacobgarner",
     "password":"welovetom",
+    "image_url":"https://mario.nintendo.com/static/5173cb58b062e1ec02553be8ed1b8019/b2548/header-mario.png"
 },
 {
     "username":"tomwest",
     "password":"welovetom",
+    "image_url": "https://upload.wikimedia.org/wikipedia/en/thumb/8/81/Wario.png/250px-Wario.png"
 },
 {
     "username":"harrydaniels",
     "password":"welovetom",
+    "image_url": "https://mario.wiki.gallery/images/thumb/3/3c/Wario_MP100.png/1200px-Wario_MP100.png"
+
 },
 {
     "username":"willprice",
     "password":"welovetom",
+    "image_url": "https://static.wikia.nocookie.net/mario/images/3/3f/SMBW_Bowser_Artwork.png/revision/latest?cb=20230910142646"
+
 },
 {
     "username":"xingmei",
     "password":"welovetom",
+    "image_url": "https://mario.wiki.gallery/images/1/1a/SMBW_Peach_Artwork.png"
+
 }]
